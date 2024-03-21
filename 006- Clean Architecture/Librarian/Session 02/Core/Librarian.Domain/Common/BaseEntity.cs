87d0e1887a7380e1051590342bdf6cea07304bbf
Foad Alavi <foadalavi@live.com>
2024-03-21 21:52:55 +0100

    Session 2 - Project Setup

---
+++ 006- Clean Architecture/Librarian/Session 02/Core/Librarian.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Librarian.Domain.Common
+{
+    public class BaseEntity
+    {
+        public int CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+
+
+        public int Id { get; set; }
+        public DateTime ModificationDate { get; set; } = DateTime.Now;
+        public int ModifiedBy { get; set; }
+    }
+}
